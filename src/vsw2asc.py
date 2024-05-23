1721824b125e943efb2c342b177cb4e54b5f9a32
Gary SHI <shihongjun@smartsct.com>
2024-05-23 16:23:59 +0800

    Add English comment

--- src/vsw2asc.py
+++ src/vsw2asc.py
@@ -39,14 +39,17 @@ from exceeddata.sdk.vdata import VDataReader, VDataByteReader
 import can
 from can import Message
 
-#本示例展示的是将存储在VSW中的原始信号帧数据输出到ASC文件中的例子
+#Input Data Sample file can be found sample_files/vsw/signal_2x2.vsw
+#Signal data should be byte array and  stored in column name like {Channel_id}_{Frame_id}
 #VSW文件中的信号名的格式为{Channel_id}_{Frame_id}，Channel id采用10进制表示，而Frame id采用0x开始的16进制表示
+#More VSW files can be added to the inputPaths list for decode.
 #示例中只有一个VSW文件，如果有多个VSW要合并转换的话，只需要在输入的文件名数组中继续添加
+#Only 4 signals selected 0_0,0_1,1_0,1_1
 #只选取了四组信号
 
-inputPaths = ['EXD_file_signal0.vsw']
+inputPaths = ['../sample_files/vsw/signal_2x2.vsw']
 seekables = []
-signals = ['3_0x268', '3_0x32c', '3_0x338', '3_0x3a2']
+signals = ['0_0', '0_1', '1_0', '1_1']
 ascFilename='pycanasc.asc'
 
 for fname in inputPaths:
@@ -58,6 +61,7 @@ reader = VDataReader(seekables, signals=signals)
 vdf = reader.df()
 
 #print(vdf.cols)
+#store the coloumn index to channel_id, message_id tuple mapping. 
 #这里用来保留信号列与channel_id, message_id之间的对应关系，第一列总是为时间，所以会少一列
 can_info= []
 for signal in vdf.cols(): 
@@ -65,6 +69,7 @@ for signal in vdf.cols():
     if (len(arr)!= 2):
         can_info.append(None)
     else:
+        #You can customize here for you own column name format.
         #这里需要做容错的处理，如果信号中有下划线，但不是数值就会出错。
         channel_id =int(arr[0])
         message_id = int (arr[1],base=16)
@@ -75,9 +80,11 @@ current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 print("  " + current_time)
 ascwriter = can.ASCWriter(ascFilename)
 
+#itorator for each row and data column.
 #遍历解出来的每一行数据
 for row in vdf.values():
     #第一列是时间，所以从第二列信号数据开始分析
+    #time for first column, so we start from the second column.
     for i in range (1, len(row)):
             #如果信号非空，则生成相应的数据
             if row[i] != None:
@@ -85,6 +92,7 @@ for row in vdf.values():
                 #写入ASC文件
                 ascwriter.on_message_received(msg)
 
+#finalize the ASC file and flush to disk
 #将ASC文件终结，写入磁盘
 ascwriter.stop()
 
